de9d2fd2d789bfce5d6188e5acb6d98e60dacac9
Chris Nelson <cnelson04@gmail.com>
2021-09-20 20:08:40 -0500

    GET and POST working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,56 @@ $( document ).ready ( onReady );
 
 function onReady(){
     console.log( 'JQuery is ready!' );
+    $( '#submitButton').on ( 'click', sendMessages );
+    let interval = setInterval( getMessages, 3000 )
+}
+
+function getMessages() {
+    console.log( 'in getMessages');
+    $.ajax({
+        method: 'GET',
+        url: '/messages'
+    }).then( function( response ) {
+        console.log( 'back from server with:', response );
+        let el = $("#messages")
+        el.empty();
+        for(let i=0; i<response.length; i++){
+            if (`${response[i].name}` === 'Chris' ){
+                console.log( 'in if statement. background change is working');
+                el.append(`<p class="textPlacmentRight"><span class="blue"><b>${response[i].name}:</b> ${response[i].message}</span></p>`);
+            } // end if
+            else {
+                console.log( 'in else statement! why is background color not changing?');
+                el.append(`<p class="textPlacmentLeft"><span class="grey"> <b>${response[i].name}:</b> ${response[i].message}</span></p>`);
+            } // end else if
+        } // end for loop
+    }).catch( function ( err ){
+        alert( 'There is an error.' );
+        console.log( 'There is an error:', err );
+        clearInterval( interval );
+    })
+}
+
+function sendMessages(){
+    console.log('in sendMessages');
+    // get user input and place in object
+        let objectToSend = {
+            name: $('#messageName').val(),
+            message: $('#messageText').val()
+        }
+        $('#messageText').val('');
+        console.log( 'sending:', objectToSend );
+    // make a POST request to server
+    $.ajax({
+        method: 'POST',
+        url: '/messages',
+        data: objectToSend
+    }).then( function( response ){
+        console.log( 'back from POST:', response );
+        // update the DOM
+        getMessages();
+    }).catch( function( err ){
+        console.log( 'Error:', err );
+        alert( 'There\'s an error!')
+    })
 }
--- server/server.js
+++ server/server.js
@@ -2,18 +2,19 @@
 let express = require ( 'express' );
 let app = express();
 
+let bodyParser = require( 'body-parser' );
+
 
 // uses
     // server static files
     app.use( express.static( 'server/public' ) ); // base folder for files
 
+    app.use( bodyParser.urlencoded({ extended: true } ) );
+
+
 // global variables
 const port = 5000
-let messages = [{
-    author: 'Heather',
-    text: 'I\'m testing the text'
-}
-];
+let messages = [];
 
 // spin up server
 app.listen( port, () => {
@@ -25,4 +26,10 @@ app.get( '/messages', (req, res) => {
     console.log( '/messages GET hit' );
     // res.send ( 'APP.GET ON /MESSAGES IS WORKING!' ); // once working, you can comment out
     res.send ( messages );
+})
+
+app.post( '/messages', (req, res ) =>{
+    console.log('/messages POST', req.body );
+    messages.push( req.body );
+    res.sendStatus( 201 );
 })
